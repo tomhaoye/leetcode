dc71ef2eeb2817f12e86bc5d7b5dbec46b0d6739
tom <tomhaoye@163.com>
2018-12-14 23:49:43 +0800

    78

--- 001~100/78.py
+++ 001~100/78.py
@@ -5,4 +5,14 @@ class Solution:
         :rtype: List[List[int]]
         """
         init = []
+        length = len(nums)
+        indicator1, indicator2 = 0, 0
+        while True:
+            if indicator1 == length and indicator2 == length:
+                break
+            elif indicator2 == length:
+                indicator1 += 1
+                indicator2 = indicator1
+            else:
+                indicator2 += 1
         return init
