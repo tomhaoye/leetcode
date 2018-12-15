08a616cfcc387a2690d3c232f3f11497a3b90ee0
tom <tomhaoye@163.com>
2018-12-15 21:32:38 +0800

    leet78

--- 001~100/78.py
+++ 001~100/78.py
@@ -1,24 +1,20 @@
 class Solution:
     def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        
-    # 递归方案一，超时
-    # def subsets(self, nums):
-    #     res,_dict = [[],nums[:]],{}
-    #     for key in self.rec(nums, _dict):
-    #         res.append([int(i) for i in key.split(' ')])
-    #     return res
-    # def rec(self, nums, _dict):
-    #     length = len(nums)
-    #     imitate = nums[:]
-    #     for i in range(length):
-    #         nums = imitate[:]
-    #         nums.pop(i)
-    #         if not nums:
-    #             break
-    #         _dict[' '.join(str(x) for x in nums)] = 1
-    #         self.rec(nums, _dict)
-    #     return _dict
+        res,_dict = [[],nums[:]],{}
+        for key in self.rec(nums, _dict):
+            res.append([int(i) for i in key.split(' ')])
+        return res
+    def rec(self, nums, _dict):
+        length = len(nums)
+        imitate = nums[:]
+        for i in range(length):
+            nums = imitate[:]
+            nums.pop(i)
+            if not nums:
+                break
+            key_word = ' '.join(str(x) for x in nums)
+            if key_word in _dict:
+                continue
+            _dict[key_word] = 1
+            self.rec(nums, _dict)
+        return _dict
