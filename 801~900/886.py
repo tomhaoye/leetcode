9659957981d0e6c6a1cad134c399b76b106d1932
tom <tomhaoye@163.com>
2018-12-17 23:48:34 +0800

    pre886

---
+++ 801~900/886.py
@@ -0,0 +1,9 @@
+class Solution:
+    def possibleBipartition(self, N, dislikes):
+        """
+        :type N: int
+        :type dislikes: List[List[int]]
+        :rtype: bool
+        """
+        
+        
