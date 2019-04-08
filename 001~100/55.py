f38b1a21e9b3ff550c2c117a589019ffed5a4c91
tom <tomhaoye@163.com>
2019-04-09 00:37:13 +0800

    55：exceed time

---
+++ 001~100/55.py
@@ -0,0 +1,15 @@
+# 呀超时了
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        length = len(nums);
+        if length <= 1:
+            return True;
+        index = 0;
+        for num in nums[:length-1]:
+            if index < length - 1 and index + num >= length - 1:
+                if index == 0:
+                    return True;
+                else:
+                    return self.canJump(nums[:index+1]);
+            index += 1;
+        return False;
