67e6d0fd3293a481c3205c7207e7f8e9285bb6f1
tom <tomhaoye@163.com>
2019-04-10 00:17:46 +0800

    55:over memory

--- 001~100/55.py
+++ 001~100/55.py
@@ -13,3 +13,21 @@ class Solution:
                     return self.canJump(nums[:index+1]);
             index += 1;
         return False;
+
+# 这次超内存了
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        length = len(nums);
+        if length <= 1:
+            return True;
+        index = length - 2;
+        for num in nums[:length-1][::-1]:
+            if index < length - 1 and index + num >= length - 1:
+                if index == 0:
+                    return True;
+                else:
+                    return self.canJump(nums[:index+1]);
+            index -= 1;
+        return False;
+
+        
