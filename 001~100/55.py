f392b205404d9e4cb77183be5929c9d4695b8154
tom <tomhaoye@163.com>
2019-04-12 01:15:20 +0800

    55

--- 001~100/55.py
+++ 001~100/55.py
@@ -1,33 +1,44 @@
 # 呀超时了
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        length = len(nums);
+        length = len(nums)
         if length <= 1:
-            return True;
-        index = 0;
+            return True
+        index = 0
         for num in nums[:length-1]:
             if index < length - 1 and index + num >= length - 1:
                 if index == 0:
-                    return True;
+                    return True
                 else:
-                    return self.canJump(nums[:index+1]);
-            index += 1;
-        return False;
+                    return self.canJump(nums[:index+1])
+            index += 1
+        return False
 
 # 这次超内存了
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        length = len(nums);
+        length = len(nums)
         if length <= 1:
-            return True;
-        index = length - 2;
+            return True
+        index = length - 2
         for num in nums[:length-1][::-1]:
             if index < length - 1 and index + num >= length - 1:
                 if index == 0:
-                    return True;
+                    return True
                 else:
-                    return self.canJump(nums[:index+1]);
-            index -= 1;
-        return False;
+                    return self.canJump(nums[:index+1])
+            index -= 1
+        return False
 
-        
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        at_least_step_need = 0
+        nums = nums[:-1][::-1]
+        for num in nums:
+            at_least_step_need += 1
+            if num >= at_least_step_need:
+                at_least_step_need = 0
+        return at_least_step_need == 0
+
+
